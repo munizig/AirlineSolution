a949773bbb3b424823c1a4d6e9830c9a04f81607
Igor Muniz <imuniz@dannemann.com.br>
2019-04-09 01:38:16 -0300

    Adding solution

---
+++ Manager.Application/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Manager.Application.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static bool HasAny<T>(this IEnumerable<T> list) where T : class
+        {
+            return list != null && list.Any();
+        }
+    }
+}
